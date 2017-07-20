4e228b0daa7a71584a4314b36ec42e89173d4959
yongmingz <zym1115718204@163.com>
2017-07-20 15:12:52 +0800

    Update Welcome Page

--- xblog/app/views.py
+++ xblog/app/views.py
@@ -4,14 +4,23 @@
 from app import app
 from flask import render_template, url_for
 
+
 @app.route('/test')
 def test():
     return "Hello World!"
 
-@app.route('/')
-def index():
-    return render_template("welcome.html",title="Welcome")
+
+# @app.route('/')
+# def disabled_index():
+#     return render_template("welcome.html", title="Welcome")
+
 
 @app.route('/home')
 def home():
-    return render_template("base.html",title="Home")
+    return render_template("base.html", title="Welcome")
+
+
+@app.route('/')
+def index():
+    return render_template("welcome.html", title="Welcome")
+
