37b12f654e8eda7fb5456c7a521d21df127ff693
yongmingz <zym1115718204@163.com>
2017-07-21 13:52:59 +0800

    Update Welcome Page

--- xblog/manage.py
+++ xblog/manage.py
@@ -6,7 +6,7 @@ from app import app
 
 manager = Manager(app)
 manager.add_command("runserver", 
-                Server(host="127.0.0.1", port=2018, use_debugger=True))
+                Server(host="127.0.0.1", port=8001, use_debugger=True))
 
 if __name__ == '__main__':
     manager.run()
