bf8d081bc673c6ce5ebe902307f9cffc2e9eda57
yongmingz <zym1115718204@163.com>
2017-07-24 01:55:01 +0800

    Add Background images to No.4-No.13

--- xblog/app/views.py
+++ xblog/app/views.py
@@ -2,17 +2,17 @@
 # -*- coding: utf-8 -*-
 
 from app import app
-from app.models import Post
+# from app.models import Post
 from flask import render_template, url_for
 
 
-# @app.route('/home')
-# def home():
-#     """
-#     Home Page
-#     :return:
-#     """
-#     return render_template("base.html", title="Welcome")
+@app.route('/home')
+def home():
+    """
+    Home Page
+    :return:
+    """
+    return render_template("base.html", title="Welcome")
 
 
 @app.route('/')
@@ -28,20 +28,20 @@ def index():
     return render_template("welcome.html", title="Welcome", background_img=background_img)
 
 
-@app.route('/home')
-def home():
-    posts = Post.objects.all()
-    return render_template('post.html', title="Home", posts=posts)
+# @app.route('/home')
+# def home():
+#     posts = Post.objects.all()
+#     return render_template('post.html', title="Home", posts=posts)
+#
 
-
-@app.route('/post/<string:title>')
-def read_more(title):
-    post = Post.objects.get_or_404(title=title)
-    return render_template('read_more.html', title="Post", post=post)
-
-
-@app.route('/archive')
-def archive():
-    posts = Post.objects.all()
-    return render_template('archive.html', title='Archive', posts=posts)
+# @app.route('/post/<string:title>')
+# def read_more(title):
+#     post = Post.objects.get_or_404(title=title)
+#     return render_template('read_more.html', title="Post", post=post)
+#
+#
+# @app.route('/archive')
+# def archive():
+#     posts = Post.objects.all()
+#     return render_template('archive.html', title='Archive', posts=posts)
 
